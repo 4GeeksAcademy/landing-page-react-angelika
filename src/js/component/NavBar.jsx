f328f5ba93ce756672f90d4371940053f6917bc8
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-05-17 19:35:15 +0000

    landing page React

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand text-white" href="#">Navbar</a>
+                <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
